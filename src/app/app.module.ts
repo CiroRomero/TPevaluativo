e7340056e2f1286df26a31fa7bba29bae5f80d4b
CiroRomero <rccirofacondo@mail.com>
2024-06-19 11:08:34 -0300

    comienzo del tp evaluativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from 'src/environments/environment'; //Vincula a la BD con la App
+import { AngularFireModule } from '@angular/fire/compat'; //Treabaja con las colecciones de informacion
+import { AngularFireAuthModule } from '@angular/fire/compat/auth' ;//Trabaja con la autentificación
+import { AngularFireStorageModule } from '@angular/fire/compat/storage' ; 
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +18,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
