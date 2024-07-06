0f66b2a3dbe432d4db2a053964a730b27c381240
CiroRomero <rccirofacondo@gmail.com>
2024-07-06 00:10:29 -0300

    arregle unas cosas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-// Son las encargadas de tener todas las rutas de la página
 const routes: Routes = [
 
-  //es una ruta comun que te lleva a un componente
   {path:"",component:InicioComponent},
-  //carga perezosa = ruta que te lleva a un modulo especifico
-  //loadChildren inidica que habra una ruta fija
-  //()=>import funcion flecha que importa desde la ruta 
-  //.then funcion asincronica del tipo PROMESA
   {
     path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
   },
--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -41,36 +41,3 @@ export class CardComponent {
 
 
 
-/*
-
-  //INICIALIZAR LA PROPIEDAD INFO
-  constructor() {
-    this.info = [
-      {
-        id: "",
-        nombre: "BICICLETA INDOOR RANDERS MAX FC-58H",
-        tipo: "Bicicleta",
-        imagen: "https://acdn.mitiendanube.com/stores/001/175/201/products/img_1-41-e6440816df346847ad16125189919184-320-0.webp",
-        descripcion: "$1.099.009,10"
-      },
-
-      {
-        id: "",
-        nombre: "CINTA MOTORIZADA LÍNEA HOGAR RANDERS ARG-366",
-        tipo: "Cinta De Correr",
-        imagen: "https://acdn.mitiendanube.com/stores/001/175/201/products/arg-3661-a5e0e69284ea048aba16597176117130-1024-10241-e57d01d21b7b3bfcaf16603096808593-320-0.webp",
-        descripcion: "$319.200"
-      },
-
-      {
-        id: "",
-        nombre: "BICICLETA DE SPINNING ATHLETIC 400BS",
-        tipo: "Bicicleta",
-        imagen: "https://acdn.mitiendanube.com/stores/001/175/201/products/400bs11-0460c5c1d2e811f9a316832945209048-320-0.webp",
-        descripcion: "$340.480"
-      },
-    ]
-  }
-
-}
-*/
