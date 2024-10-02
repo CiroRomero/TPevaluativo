13c150039cd63d3e9a5fd33cd9beb6aa0aa7faeb
CiroRomero <rccirofacondo@gmail.com>
2024-10-02 09:11:11 -0300

    por las dudas

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -17,11 +17,31 @@ export class CardComponent {
         ruta: "/indumentaria",
         imagen: "https://i.pinimg.com/originals/83/bc/c5/83bcc5cbb57b9b6d20ed6eadf48686d6.jpg",
         carruselCards: [
-          { nombre: "Card 1", ruta: "/card1", imagen: "https://i.pinimg.com/originals/83/bc/c5/83bcc5cbb57b9b6d20ed6eadf48686d6.jpg" },
-          { nombre: "Card 2", ruta: "/card2", imagen: "https://i.pinimg.com/originals/e3/7b/8b/e37b8bf52b029f4e9d6bbd907a0e5e4c.jpg" },
-          { nombre: "Card 3", ruta: "/card3", imagen: "https://th-test-11.slatic.net/p/6e4cd8601a6e75f229adf69fdcd91a94.jpg" },
-          { nombre: "Card 4", ruta: "/card4", imagen: "https://th-test-11.slatic.net/p/6e4cd8601a6e75f229adf69fdcd91a94.jpg" },
-          { nombre: "Card 5", ruta: "/card5", imagen: "https://th-test-11.slatic.net/p/6e4cd8601a6e75f229adf69fdcd91a94.jpg" }
+          { 
+            nombre: "Card 1", 
+            ruta: "/card1", 
+            imagen: "https://sporting.vtexassets.com/arquivos/ids/1117673-800-800?v=638460101366670000&width=800&height=800&aspect=true" 
+          },
+          { 
+            nombre: "Card 2", 
+            ruta: "/card2", 
+            imagen: "https://sporting.vtexassets.com/arquivos/ids/1454880-800-800?v=638633917330670000&width=800&height=800&aspect=true" 
+          },
+          { 
+            nombre: "Card 3", 
+            ruta: "/card3", 
+            imagen: "https://sporting.vtexassets.com/arquivos/ids/952734-800-800?v=638325386672400000&width=800&height=800&aspect=true" 
+          },
+          { 
+            nombre: "Card 4", 
+            ruta: "/card4", 
+            imagen: "https://sporting.vtexassets.com/arquivos/ids/1330769-800-800?v=638558039289430000&width=800&height=800&aspect=true" 
+          },
+          {
+             nombre: "Card 5", 
+             ruta: "/card5", 
+             imagen: "https://sporting.vtexassets.com/arquivos/ids/1028605-800-800?v=638386692711800000&width=800&height=800&aspect=true" 
+            }
         ]
       },
       // Agrega los otros objetos siguiendo el mismo formato...
